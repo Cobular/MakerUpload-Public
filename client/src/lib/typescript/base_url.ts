c48facd2281c3346a150ca16b3701c0e14ed92c0
cobular <22972550+Cobular@users.noreply.github.com>
2023-03-09 21:36:44 -0800

    Fix base url

--- client/src/lib/typescript/base_url.ts
+++ client/src/lib/typescript/base_url.ts
@@ -1,2 +1,2 @@
-// export const BASE_URL = "https://cf-worker.cobular.workers.dev/files?"
-export const BASE_URL = "http://127.0.0.1:8787/files?"
+export const BASE_URL = "https://cf-worker.cobular.workers.dev/files?"
+// export const BASE_URL = "http://127.0.0.1:8787/files?"
