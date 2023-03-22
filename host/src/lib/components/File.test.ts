d11fabc55ac9c48d547d7f5bc2951221ceb1176e
cobular <22972550+Cobular@users.noreply.github.com>
2023-03-21 20:04:02 -0700

    Add vitest

---
+++ host/src/lib/components/File.test.ts
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/svelte';
+import type { DocumentData } from 'makersync-common/types';
+import DefaultProps from './File.svelte';
+
+const weird_js_object = {
+  toDate: () => new Date()
+}
+
+it('set and update prop', async () => {
+	const file: DocumentData = {
+		creation_time: weird_js_object,
+		download_url: 'https://cf-worker.cobular.workers.dev/files?uuid=1234',
+		target_machine: 'Sewing',
+		name: 'test file',
+		uid: '1234'
+	};
+	const { component } = render(DefaultProps, { file });
+
+	expect(screen.queryByText('test file')).toBeInTheDocument();
+
+	// Update prop using Svelte's Client-side component API
+  const file2: DocumentData = {
+		creation_time: weird_js_object,
+		download_url: 'https://cf-worker.cobular.workers.dev/files?uuid=1234',
+		target_machine: 'Sewing',
+		name: 'two',
+		uid: '1234'
+	};
+	await component.$set({ file: file2 });
+	expect(screen.queryByText('two')).toBeInTheDocument();
+});
--- host/vite.config.ts
+++ host/vite.config.ts
@@ -1,9 +1,31 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
+import type { UserConfig } from 'vite';
+import { configDefaults, type UserConfig as VitestConfig } from 'vitest/config';
+const config: UserConfig & { test: VitestConfig['test'] } = {
 	plugins: [sveltekit()],
 	server: {
 		port: 3000
+	},
+	define: {
+		// Eliminate in-source test code
+		'import.meta.vitest': 'undefined'
+	},
+	test: {
+		// jest like globals
+		globals: true,
+		environment: 'jsdom',
+		// in-source testing
+		includeSource: ['src/**/*.{js,ts,svelte}'],
+		// Add @testing-library/jest-dom matchers & mocks of SvelteKit modules
+		setupFiles: ['./setupTest.ts'],
+		// Exclude files in c8
+		coverage: {
+			exclude: ['setupTest.ts']
+		},
+		// Exclude playwright tests folder
+		exclude: [...configDefaults.exclude, 'tests']
 	}
-});
+};
+
+export default config;
