5d9e914917f0dd90432b2ef2ef0244cdce2bdd00
cobular <22972550+Cobular@users.noreply.github.com>
2023-03-21 17:57:39 -0700

    Worker ready to go!

--- cf-worker/src/index.ts
+++ cf-worker/src/index.ts
@@ -1,5 +1,6 @@
-import {Root} from "./routes/page";
-import {Files} from "./routes/files/page";
+import { Root } from "./routes/page";
+import { Files } from "./routes/files/page";
+import { Toucan } from "toucan-js";
 
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -17,6 +18,25 @@ export interface Env {
   FIREBASE_PROJECT_ID: string;
   FIREBASE_SERVICE_ACCT_JSON: string;
   TURNSTILE_KEY: string;
+  SENTRY_DSN: string;
+}
+
+async function handler(
+  request: Request,
+  env: Env,
+  ctx: ExecutionContext
+): Promise<Response> {
+  const url = new URL(request.url);
+
+  switch (url.pathname) {
+    case "/":
+      return Root(url, request, env, ctx);
+      break;
+    case "/files":
+      return Files(url, request, env, ctx);
+    default:
+      return new Response("Not Found", { status: 404 });
+  }
 }
 
 export default {
@@ -25,16 +45,21 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    const url = new URL(request.url);
+    const sentry = new Toucan({
+      dsn: env.SENTRY_DSN,
+      release: "1.0.0",
+      context: ctx,
+      request,
+    });
 
-    switch (url.pathname) {
-      case "/": 
-				return Root(url, request, env, ctx);
-				break
-      case "/files":
-        return Files(url, request, env, ctx);
-			default:
-				return new Response("Not Found", { status: 404 });
+    try {
+      return handler(request, env, ctx);
+    } catch (e) {
+      sentry.captureException(e);
+
+      return new Response("Something went wrong! Team has been notified.", {
+        status: 500,
+      });
     }
   },
 };
--- cron-worker/src/index.ts
+++ cron-worker/src/index.ts
@@ -11,6 +11,7 @@
  */
 
 import { FirestoreInterface } from "makersync-common/firestore";
+import { Toucan } from "toucan-js";
 
 export interface Env {
   // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -25,7 +26,65 @@ export interface Env {
 
   FIREBASE_PROJECT_ID: string;
   FIREBASE_SERVICE_ACCT_JSON: string;
+  SENTRY_DSN: string;
 }
+
+async function handler(
+  controller: ScheduledController,
+  env: Env,
+  ctx: ExecutionContext
+) {
+  // Get a list of all entries in firebase older than 2 minutes
+  const firestore = await FirestoreInterface.New(
+    env.FIREBASE_PROJECT_ID,
+    env.FIREBASE_SERVICE_ACCT_JSON
+  );
+
+  const documents = await firestore.getCollection("files");
+
+  console.log("there are " + documents.length + " documents in firebase");
+
+  const now = new Date();
+
+  const docs_to_delete = documents.filter((doc) => {
+    const diff = now.getTime() - Date.parse(doc.createTime);
+    // 2 minutes ago
+    return diff > 1000 * 60 * 2;
+  });
+
+  if (docs_to_delete.length === 0) {
+    console.log("No documents to delete");
+    return;
+  }
+
+  // Delete all entries in R2
+  const document_ids = docs_to_delete.map((doc) => doc.fields.uid.stringValue);
+  await env.FILE_CACHE_BUCKET.delete(document_ids);
+  await env.PREVIEW_FILE_CACHE_BUCKET.delete(document_ids);
+  console.log("Deleted all entries in R2");
+
+  // Delete all entries in firebase
+  const document_refs = docs_to_delete.map((doc) => doc.name);
+  console.log("Deleting " + document_refs.length + " documents from firebase");
+
+  await Promise.all(
+    document_refs.map(async (ref) => {
+      try {
+        const response = await firestore.deleteDocumentInCollectionRaw(ref);
+        console.log("Deleted " + ref);
+      } catch (e) {
+        console.error(e);
+      }
+    })
+  );
+}
+
+const headers = {
+  Authorization:
+    "DSN https://bac4d51d022046fb8e5411656a8fbbc3@o4504879295627264.ingest.sentry.io/4504879441969152",
+};
+const monitor_id = "526026b2-88c3-45fa-9726-bba84b061761"; // Write your monitor_id here
+const org_slug = "ucla-makerspace"; // Write your organization slug here
 
 export default {
   async scheduled(
@@ -33,52 +92,47 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<void> {
-    // Get a list of all entries in firebase older than 2 minutes
-    const firestore = await FirestoreInterface.New(
-      env.FIREBASE_PROJECT_ID,
-      env.FIREBASE_SERVICE_ACCT_JSON
-    );
-
-    const documents = await firestore.getCollection("files");
-
-    console.log("there are " + documents.length + " documents in firebase");
-
-    const now = new Date();
-
-    const docs_to_delete = documents.filter((doc) => {
-      const diff = now.getTime() - Date.parse(doc.createTime);
-      // 2 minutes ago
-      return diff > 1000 * 60 * 2;
+    const sentry = new Toucan({
+      dsn: env.SENTRY_DSN,
+      release: "1.0.0",
+      context: ctx,
     });
 
-    if (docs_to_delete.length === 0) {
-      console.log("No documents to delete");
-      return;
-    }
-
-    // Delete all entries in R2
-    const document_ids = docs_to_delete.map(
-      (doc) => doc.fields.uid.stringValue
-    );
-    await env.FILE_CACHE_BUCKET.delete(document_ids);
-    await env.PREVIEW_FILE_CACHE_BUCKET.delete(document_ids);
-    console.log("Deleted all entries in R2");
-
-    // Delete all entries in firebase
-    const document_refs = docs_to_delete.map((doc) => doc.name);
-    console.log(
-      "Deleting " + document_refs.length + " documents from firebase"
-    );
-
-    await Promise.all(
-      document_refs.map(async (ref) => {
-        try {
-          const response = await firestore.deleteDocumentInCollectionRaw(ref);
-          console.log("Deleted " + ref);
-        } catch (e) {
-          console.error(e);
+    try {
+      // Create the check-in
+      var startDate = new Date();
+      const json_data = { status: "in_progress" };
+      const create_checkin_response = await fetch(
+        `https://sentry.io/api/0/organizations/${org_slug}/monitors/${monitor_id}/checkins/`,
+        {
+          method: "POST",
+          headers: headers,
+          body: JSON.stringify(json_data),
         }
-      })
-    );
+      );
+      if (!create_checkin_response.ok) {
+        throw new Error(
+          `HTTP error! Status: ${create_checkin_response.status}`
+        );
+      }
+
+      handler(controller, env, ctx);
+
+      var endDate = new Date();
+      const update_data = {
+        status: "ok",
+        duration: endDate.getTime() - startDate.getTime(),
+      };
+      const update_checkin_response = await fetch(
+        `https://sentry.io/api/0/organizations/${org_slug}/monitors/${monitor_id}/checkins/latest/`,
+        {
+          method: "PUT",
+          headers: headers,
+          body: JSON.stringify(update_data),
+        }
+      );
+    } catch (e) {
+      sentry.captureException(e);
+    }
   },
 };
