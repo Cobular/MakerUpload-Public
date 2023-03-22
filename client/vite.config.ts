2769be0388c563e628a9f40bc3beae24b859a1e0
cobular <22972550+Cobular@users.noreply.github.com>
2023-03-21 17:21:39 -0700

    Add release detection

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,27 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	build: {
+		sourcemap: true
+	},
+	plugins: [
+		sveltekit(),
+		// Put the Sentry vite plugin after all other plugins
+		sentryVitePlugin({
+			org: 'ucla-makerspace',
+			project: 'user-client',
+
+			// Specify the directory containing build artifacts
+			include: './dist',
+
+			// Auth tokens can be obtained from https://sentry.io/settings/account/api/auth-tokens/
+			// and need `project:releases` and `org:read` scopes
+			authToken: process.env.SENTRY_AUTH_TOKEN
+
+			// Optionally uncomment the line below to override automatic release name detection
+			// release: process.env.RELEASE,
+		})
+	]
 });
